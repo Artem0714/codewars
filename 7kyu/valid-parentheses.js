a333b10f4265b7ac167482ef596aed32786692bb
Artem0714 <arfed96@gmail.com>
2023-03-30 17:22:45 +0300

    added link for tasks

--- 7kyu/valid-parentheses.js
+++ 7kyu/valid-parentheses.js
@@ -1,3 +1,5 @@
+//Link: https://www.codewars.com/kata/6411b91a5e71b915d237332d
+
 // DESCRIPTION:
 // Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
