bd449e6612b163f93dbf7a819b19aaa809b8e733
Artem0714 <arfed96@gmail.com>
2023-03-30 17:24:28 +0300

    added links for tasks

--- 7kyu/divide-and-conquer.js
+++ 7kyu/divide-and-conquer.js
@@ -1,3 +1,5 @@
+//Link: https://www.codewars.com/kata/57eaec5608fed543d6000021/javascript
+
 // Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
 
 // Return as a number.
